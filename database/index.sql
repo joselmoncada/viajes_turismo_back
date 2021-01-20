2c87b1a2976517394aa4b2afc6653c2100cddc22
Jose Luis <joseluismoncadaostos@gmail.com>
2021-01-20 18:06:25 -0400

    delete index.sql

--- database/index.sql
+++
@@ -1,8 +0,0 @@
-CREATE DATABASE viajes;
-
-CREATE TABLE region(
-    id SERIAL PRIMARY KEY,
-    nombre VARCHAR(40) NOT NULL
-);
-
-INSERT INTO region(nombre) VALUES ('Asia'), ('Europa'), ('Oceania'), ('Latinoamerica'), ('Norteamerica');
