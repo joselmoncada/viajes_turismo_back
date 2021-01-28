70d650abd252c72dd6bd2c8f815c8120eff605f2
vicalex96 <vadjdn@gmail.com>
2021-01-28 14:25:09 -0400

    haciendo mas leible el rally.controller

--- src/controllers/rallies.controller.js
+++ src/controllers/rallies.controller.js
@@ -3,7 +3,20 @@ pool = DB.getPool()
 
 const getRallies = async (req, res) => {
 
-    const response = await pool.query('SELECT * FROM CJV_rally as r left join (Select Distinct id_rally from CJV_participacion ) as p on r.id = p.id_rally left join (Select Distinct id_rally as id_rally_2 from CJV_organizador ) as o on r.id = o.id_rally_2 left join (Select Distinct id_rally as id_rally_3 from CJV_circuito) as c on r.id = c.id_rally_3 left join (Select Distinct id_rally as id_rally_4 from CJV_premio) as pr on r.id = pr.id_rally_4 left join (Select Distinct id_rally as id_rally_5 from CJV_valoracion) as v on r.id = v.id_rally_5 order by r.id;');
+    const response = await pool.query(`
+        SELECT * FROM CJV_rally as r 
+            left join (
+                Select Distinct id_rally f
+                rom CJV_participacion ) as p on r.id = p.id_rally 
+            left join (Select Distinct id_rally as id_rally_2 
+                from CJV_organizador ) as o on r.id = o.id_rally_2 
+            left join (Select Distinct id_rally as id_rally_3 
+                from CJV_circuito) as c on r.id = c.id_rally_3 
+            left join (Select Distinct id_rally as id_rally_4 
+                from CJV_premio) as pr on r.id = pr.id_rally_4 
+            left join (Select Distinct id_rally as id_rally_5 
+                from CJV_valoracion) as v on r.id = v.id_rally_5 
+        order by r.id;`);
 
         
     res.status(200).json(response.rows);
@@ -11,7 +24,7 @@ const getRallies = async (req, res) => {
 
 const getRally = async (req, res) => {
 
-    const response = await pool.query('SELECT * FROM CJV_rally WHERE id = ' + req.params.id + ';');
+    const response = await pool.query('SELECT * FROM CJV_rally WHERE id = $1;',[req.params.id]);
 
     console.log(response.rows);
 
@@ -20,7 +33,13 @@ const getRally = async (req, res) => {
 
 const getParticipantes = async (req, res) => {
     try {
-    const response = await pool.query('Select p.id, p.num_participacion, p.puesto_final, p.fecha_culminacion, p.id_viajero, c.documento from cjv_participacion as p left join cjv_cliente as c on p.id_cliente = c.id WHERE id_rally =' + req.params.id + ';');
+    const response = await pool.query(`
+        Select p.id, p.num_participacion, p.puesto_final, 
+            p.fecha_culminacion, p.id_viajero, c.documento 
+        from cjv_participacion as p 
+        left join cjv_cliente as c on p.id_cliente = c.id 
+        WHERE id_rally = $1;`,
+        [req.params.id]);
     console.log(response.rows);
 
     res.status(200).json(response.rows);
@@ -31,7 +50,12 @@ const getParticipantes = async (req, res) => {
 
 const getOrganizadores = async (req, res) => {
     try {
-    const response = await pool.query('SELECT id_agencia, id_rally, num_cupos, nombre FROM CJV_organizador left join CJV_agencia on id_agencia = id where id_rally =' +  req.params.id + ';');
+    const response = await pool.query(`
+        SELECT id_agencia, id_rally, num_cupos, nombre 
+        FROM CJV_organizador 
+        left join CJV_agencia on id_agencia = id where id_rally = $1 ;`,
+        [req.params.id ]);
+
     console.log(response.rows);
 
     res.status(200).json(response.rows);
@@ -42,7 +66,11 @@ const getOrganizadores = async (req, res) => {
 
 const getPremios = async (req, res) => {
     try {
-    const response = await pool.query('SELECT id, nombre, puesto_destino, detalle FROM CJV_premio where id_rally =' + req.params.id + 'order by puesto_destino;');
+    const response = await pool.query( `
+        SELECT id, nombre, puesto_destino, detalle 
+        FROM CJV_premio where id_rally =$1 
+        order by puesto_destino;`, 
+        [req.params.id]);
     console.log(response.rows);
 
     res.status(200).json(response.rows);
@@ -56,8 +84,10 @@ const createRally = async (req, res) => {
 
         const {id,nombre,fecha_inicio,fecha_limite,max_num_cupos,modalidad,costo_participacion,informacion} = req.body;
         console.log('Rally: '+JSON.stringify(req.body))
-        const response = await pool.query("INSERT INTO CJV_RALLY (id,nombre,fecha_inicio,fecha_limite,max_num_cupos,modalidad,costo_participacion,informacion)"+
-        "VALUES (NEXTVAL('cjv_s_rally'),$1,$2,$3,$4,$5,$6,$7)", [nombre,fecha_inicio,fecha_limite,max_num_cupos, modalidad,costo_participacion,informacion]);
+        const response = await pool.query(`
+            INSERT INTO CJV_RALLY (id,nombre,fecha_inicio,fecha_limite,max_num_cupos,modalidad,costo_participacion,informacion)
+            VALUES (NEXTVAL('cjv_s_rally'),$1,$2,$3,$4,$5,$6,$7)`, 
+            [nombre,fecha_inicio,fecha_limite,max_num_cupos, modalidad,costo_participacion,informacion]);
         
         res.status(200).json("Rally :" + JSON.stringify(req.body) + " creado");
     } catch (error) {        
@@ -70,8 +100,10 @@ const createPremio = async (req, res) => {
 
         const {id_rally,nombre,puesto_destino,detalle} = req.body;
         console.log('Rally: '+JSON.stringify(req.body))
-        const response = await pool.query("INSERT INTO CJV_premio (id_rally,id,nombre,puesto_destino,detalle)"+
-        "VALUES ($1,NEXTVAL('cjv_s_premio'),$2,$3,$4)", [id_rally,nombre,puesto_destino,detalle]);
+        const response = await pool.query(`
+            INSERT INTO CJV_premio (id_rally,id,nombre,puesto_destino,detalle)
+            VALUES ($1,NEXTVAL('cjv_s_premio'),$2,$3,$4)`, 
+            [id_rally,nombre,puesto_destino,detalle]);
         
         res.status(200).json("Premio :" + JSON.stringify(req.body) + " creado");
     } catch (error) {        
@@ -83,8 +115,8 @@ const deleteRally = async (req, res) => {
     try {
         
         console.log('Rally: ' + req.params.id);
-        console.log("DELETE FROM CJV_rally WHERE id = " + req.params.id + ";");
-        const response = await pool.query("DELETE FROM CJV_rally WHERE id = " + req.params.id + ";");
+        console.log("DELETE FROM CJV_rally WHERE id = $1;",[ req.params.id ]);
+        const response = await pool.query("DELETE FROM CJV_rally WHERE id = $1;",[req.params.id]);
         console.log(response);
         res.status(200).json("Rally con el id: " + req.params.id + " eliminado");
     } catch (error) {
@@ -97,7 +129,7 @@ const deleteOrganizador = async (req, res) => {
         
         console.log('Organizador: ' + req.params.id);
         
-        const response = await pool.query("DELETE FROM CJV_organizador WHERE id_agencia = " + req.params.id + ";");
+        const response = await pool.query("DELETE FROM CJV_organizador WHERE id_agencia = $1;",[req.params.id]);
         console.log(response);
         res.status(200).json("Organizador eliminado");
     } catch (error) {
@@ -110,7 +142,7 @@ const deletePremio = async (req, res) => {
         
         console.log('Premio: ' + req.params.id);
         
-        const response = await pool.query("DELETE FROM CJV_premio WHERE id = " + req.params.id + ";");
+        const response = await pool.query("DELETE FROM CJV_premio WHERE id = $1;",[req.params.id]);
         console.log(response);
         res.status(200).json("Premio con el id: " + req.params.id + " eliminado");
     } catch (error) {
@@ -123,7 +155,7 @@ const deleteParticipante = async (req, res) => {
         
         console.log('Participacion: ' + req.params.id);
         
-        const response = await pool.query("DELETE FROM CJV_participacion WHERE id = " + req.params.id + ";");
+        const response = await pool.query("DELETE FROM CJV_participacion WHERE id = $1;",[req.params.id]);
         console.log(response);
         res.status(200).json("Participacion con el id: " + req.params.id + " eliminado");
     } catch (error) {
